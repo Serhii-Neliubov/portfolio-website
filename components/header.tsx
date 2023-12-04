4541d02e85b754549cbb6dc8e1529ae72e12893a
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-12-04 23:05:54 +0100

    header styles + responsive

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { links } from "@/lib/data";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="z-[999] relative">
-      <div className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] background-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"></div>
+      <div className="fixed top-0 -translate-x-[50%] left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] background-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"></div>
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {links.map((link) => {
+            return (
+              <li
+                className="h-3/4 flex items-center justify-center"
+                key={link.hash}
+              >
+                <Link
+                  className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 tra transition duration-[0.3s]"
+                  href={link.hash}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </header>
   );
 }
